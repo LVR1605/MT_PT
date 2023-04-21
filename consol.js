d65ec7c34a639875b4110ee30066f49b5e36b689
lvr1605 <dolphinlvr1605@gmail.com>
2023-04-21 23:52:34 +0800

    first commit

---
+++ consol.js
@@ -0,0 +1,6 @@
+let number = prompt("Enter a number:");
+if(number < 100) {
+  alert("You entered a number less than 100.");
+} else {
+  alert("You entered a number greater than or equal to 100.");
+}
